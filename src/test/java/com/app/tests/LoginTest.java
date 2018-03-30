78f27c3bb39888c73b69ed957946ef486d769f74
MinakovaM <m.o.rodionova@gmail.com>
2018-03-30 19:17:38 +0300

    всё ещё с костылями, но они скоро закончатся

--- src/test/java/com/app/tests/LoginTest.java
+++ src/test/java/com/app/tests/LoginTest.java
@@ -9,15 +9,22 @@ public class LoginTest extends BaseTest {
 
     @Test(dataProvider = "invalidData", priority = 0)
     public void testInvalidLogin(String login, String passwd, String errorMessage) {
-        Assert.assertEquals(app.login().invalidLoginError(login,passwd), errorMessage);
+        Assert.assertEquals(app.login().invalidLoginError(login, passwd), errorMessage);
     }
 
     @DataProvider(name = "invalidData")
     private Object[][] getLoginData() {
 
-        return app.loginsForTest();
-
+        return new Object[][]{{"testt@gmail.com", "jgjd", "Invalid password."},
+                {"jfdkjvnjfdkjbjkbf@xhdgljps.po", "ponjn", "Authentication failed."},
+                {"", "123", "An email address required."},
+                {"testt@gmail.com", "", "Password is required."},
+                {"kopj", "12369874", "Invalid email address."}};
     }
+//        return app.loginsForTest();
+//}
+
+
 
     @Test (priority = 1)
     public void testCorrectLogin() {
